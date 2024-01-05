9ac6b465c9958c78efa999e0d995bb7ad5507fd2
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-01-05 17:54:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,17 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:contacts_buddy/HomePage.dart';
-import 'package:contacts_buddy/DatabaseOp.dart';
-import 'package:contacts_buddy/AddPage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+debugShowCheckedModeBanner: false,
       title: 'Contact Buddy',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
